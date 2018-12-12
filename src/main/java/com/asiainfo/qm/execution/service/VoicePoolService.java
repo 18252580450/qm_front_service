05ae367b50cb13741ab5bf99a1d31ed646d49663
wangcheng <wangcheng@asiainfo.com>
2018-12-12 20:34:51 +0800

    质检池

---
+++ src/main/java/com/asiainfo/qm/execution/service/VoicePoolService.java
@@ -0,0 +1,16 @@
+package com.asiainfo.qm.execution.service;
+
+import com.asiainfo.qm.execution.domain.VoicePool;
+import com.asiainfo.qm.execution.vo.VoicePoolResponse;
+import java.util.List;
+import java.util.Map;
+
+
+public interface VoicePoolService {
+
+    VoicePoolResponse selectByParams(Map params, int start, int limit)throws Exception;
+
+    VoicePoolResponse update(List<String> idList) throws Exception;
+
+    VoicePoolResponse updateCheck(List<Map> list) throws Exception;
+}
