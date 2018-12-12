1d6a11749c7442e19080e733d4722c05cfbfe64c
wangcheng <wangcheng@asiainfo.com>
2018-12-12 20:37:45 +0800

    zhijian

---
+++ src/main/java/com/asiainfo/qm/execution/dao/VoicePoolMapper.java
@@ -0,0 +1,37 @@
+package com.asiainfo.qm.execution.dao;
+
+import com.asiainfo.qm.execution.domain.VoicePool;
+import com.asiainfo.qm.execution.domain.VoicePoolExample;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface VoicePoolMapper {
+    int countByExample(VoicePoolExample example);
+
+    int deleteByExample(VoicePoolExample example);
+
+    int deleteByPrimaryKey(String touchId);
+
+    int insert(VoicePool record);
+
+    int insertSelective(VoicePool record);
+
+    List<VoicePool> selectByExample(VoicePoolExample example);
+
+    VoicePool selectByPrimaryKey(String touchId);
+
+    int updateByExampleSelective(@Param("record") VoicePool record, @Param("example") VoicePoolExample example);
+
+    int updateByExample(@Param("record") VoicePool record, @Param("example") VoicePoolExample example);
+
+    int updateByPrimaryKeySelective(VoicePool record);
+
+    int updateByPrimaryKey(VoicePool record);
+
+    int update(List<String> list);//新增方法
+
+    int updateCheck(List<Map> list);//新增方法
+
+}
---
+++ src/main/java/com/asiainfo/qm/execution/dao/WorkformPoolMapper.java
@@ -0,0 +1,33 @@
+package com.asiainfo.qm.execution.dao;
+
+import com.asiainfo.qm.execution.domain.WorkformPool;
+import com.asiainfo.qm.execution.domain.WorkformPoolExample;
+import com.asiainfo.qm.execution.domain.WorkformPoolKey;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface WorkformPoolMapper {
+    int countByExample(WorkformPoolExample example);
+
+    int deleteByExample(WorkformPoolExample example);
+
+    int deleteByPrimaryKey(WorkformPoolKey key);
+
+    int insert(WorkformPool record);
+
+    int insertSelective(WorkformPool record);
+
+    List<WorkformPool> selectByExample(WorkformPoolExample example);
+
+    WorkformPool selectByPrimaryKey(WorkformPoolKey key);
+
+    int updateByExampleSelective(@Param("record") WorkformPool record, @Param("example") WorkformPoolExample example);
+
+    int updateByExample(@Param("record") WorkformPool record, @Param("example") WorkformPoolExample example);
+
+    int updateByPrimaryKeySelective(WorkformPool record);
+
+    int updateByPrimaryKey(WorkformPool record);
+
+    int update(List<String> list);//新增方法
+}
