8ef51e9c56ca847831229d25c10be9d68a761ab1
wangcheng <wangcheng@asiainfo.com>
2019-03-27 09:33:04 +0800

    配置用户权限

---
+++ src/main/java/com/asiainfo/qm/execution/vo/UserPermissionServiceResponse.java
@@ -0,0 +1,67 @@
+package com.asiainfo.qm.execution.vo;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.asiainfo.qm.manage.util.ServiceConstant;
+import com.asiainfo.qm.manage.util.TxidUtils;
+import com.asiainfo.qm.manage.vo.ServiceResponseParent;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@EqualsAndHashCode(callSuper=false)
+@Data
+@ApiModel
+public class UserPermissionServiceResponse extends ServiceResponseParent{
+    private static Logger logger = LoggerFactory.getLogger(UserPermissionServiceResponse.class);
+
+    @ApiModelProperty(name = "RSP", value = "服务返回业务数据", required = true)
+    @JSONField(name = "RSP")
+    @JsonProperty("RSP")
+    private UserPermissionResponse response;
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    public static void setLogger(Logger logger) {
+        UserPermissionServiceResponse.logger = logger;
+    }
+
+    public UserPermissionResponse getResponse() {
+        return response;
+    }
+
+    public void setResponse(UserPermissionResponse response) {
+        this.response = response;
+    }
+
+    public UserPermissionServiceResponse() {
+        try {
+            this.txid = TxidUtils.generateTxid();
+        } catch (Exception e) {
+            logger.error("can not get PtxId:", e);
+        }
+    }
+
+    public UserPermissionServiceResponse getSuccessResponse(UserPermissionResponse response) {
+        this.setStatus(ServiceConstant.STATUS_SUCCESS);
+        this.setMsg(ServiceConstant.MSG_SUCCESS);
+        this.setResponse(response);
+        this.setTxid(TxidUtils.generateTxid());
+        return this;
+    }
+
+    public UserPermissionServiceResponse getErrorResponse(String errorStatus, String errorMsg, UserPermissionResponse response) {
+        this.setStatus(errorStatus);
+        this.setMsg(errorMsg);
+        this.setResponse(response);
+       this.setTxid(TxidUtils.generateTxid());
+        return this;
+    }
+ 
+
+}
